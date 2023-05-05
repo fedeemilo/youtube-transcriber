ddec8b9e8e17300be1f616f0841d5e0c0502d178
Federico Milone <federico.milone@claro.com.ar>
2023-05-05 15:38:02 -0300

    Add vercel build

--- next.config.js
+++ next.config.js
@@ -1,6 +1,5 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  reactStrictMode: true,
+module.exports = {
+    reactStrictMode: true,
+    basePath: '',
+    trailingSlash: true
 }
-
-module.exports = nextConfig
