40a1b80bcc456ca096d0f42624fd17e50bf13e0a
Federico Milone <federico.milone@claro.com.ar>
2023-05-06 15:59:25 -0300

    Add vercel build

--- pages/api/transcript.ts
+++ pages/api/transcript.ts
@@ -4,21 +4,21 @@ import path from 'path'
 import { transferChildProcessOutput } from '../../utils/shell'
 
 export default function GET(
-  request: NextApiRequest,
-  response: NextApiResponse
+    request: NextApiRequest,
+    response: NextApiResponse
 ) {
-  const video_id = request.query.video_id as string
-  if (typeof video_id !== 'string') {
-    response.status(400).json({ error: 'Invalid request' })
-    return
-  }
+    const video_id = request.query.video_id as string
+    if (typeof video_id !== 'string') {
+        response.status(400).json({ error: 'Invalid request' })
+        return
+    }
 
-  console.log('Video ID:', video_id)
+    console.log('Video ID:', video_id)
 
-  const cmd = spawn('python3', [
-    path.join(process.cwd(), 'scripts/transcribe.py'),
-    video_id || ''
-  ])
+    const cmd = spawn('python', [
+        path.join(process.cwd(), 'scripts/transcribe.py'),
+        video_id || ''
+    ])
 
-  transferChildProcessOutput(cmd, response)
+    transferChildProcessOutput(cmd, response)
 }
--- pages/api/translate.ts
+++ pages/api/translate.ts
@@ -4,25 +4,25 @@ import path from 'path'
 import { transferChildProcessOutput } from '../../utils/shell'
 
 export default function POST(
-  request: NextApiRequest,
-  response: NextApiResponse
+    request: NextApiRequest,
+    response: NextApiResponse
 ) {
-  const srt = request.body
-  if (typeof srt !== 'string') {
-    response.status(400).json({ error: 'Invalid request' })
-    return
-  }
-
-  const cmd = spawn(
-    'python3',
-    [path.join(process.cwd(), 'scripts/translate.py')],
-    {
-      cwd: process.cwd()
+    const srt = request.body
+    if (typeof srt !== 'string') {
+        response.status(400).json({ error: 'Invalid request' })
+        return
     }
-  )
 
-  cmd.stdin.write(srt)
-  cmd.stdin.end()
+    const cmd = spawn(
+        'python',
+        [path.join(process.cwd(), 'scripts/translate.py')],
+        {
+            cwd: process.cwd()
+        }
+    )
 
-  transferChildProcessOutput(cmd, response)
+    cmd.stdin.write(srt)
+    cmd.stdin.end()
+
+    transferChildProcessOutput(cmd, response)
 }
