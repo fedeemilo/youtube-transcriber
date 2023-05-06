bd573d5cf497e71187c56731fde1d1265774b9f6
Federico Milone <federico.milone@claro.com.ar>
2023-05-06 16:06:18 -0300

    Add vercel build

--- pages/api/transcript.ts
+++ pages/api/transcript.ts
@@ -1,5 +1,5 @@
 import type { NextApiRequest, NextApiResponse } from 'next'
-import { spawn } from 'child_process'
+import { spawn, execSync } from 'child_process'
 import path from 'path'
 import { transferChildProcessOutput } from '../../utils/shell'
 
@@ -14,6 +14,13 @@ export default function GET(
     }
 
     console.log('Video ID:', video_id)
+
+    // Obtener la ruta completa del intérprete de Python
+    const pythonPath = execSync('which python').toString().trim()
+
+    // Utilizar la variable `pythonPath` en tu código
+    console.log(`Ruta de Python: ${pythonPath}`)
+    // ...resto de tu código utilizando `pythonPath`...
 
     const cmd = spawn('python', [
         path.join(process.cwd(), 'scripts/transcribe.py'),
