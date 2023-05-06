6bc572d319ba2f4a0d686012ead1ed212a33fd8b
Federico Milone <federico.milone@claro.com.ar>
2023-05-06 16:10:14 -0300

    Add vercel build

--- pages/api/transcript.ts
+++ pages/api/transcript.ts
@@ -1,7 +1,9 @@
 import type { NextApiRequest, NextApiResponse } from 'next'
-import { spawn, execSync } from 'child_process'
+import { spawn } from 'child_process'
 import path from 'path'
 import { transferChildProcessOutput } from '../../utils/shell'
+
+const ENV = process.env as any
 
 export default function GET(
     request: NextApiRequest,
@@ -15,14 +17,22 @@ export default function GET(
 
     console.log('Video ID:', video_id)
 
-    // Obtener la ruta completa del intérprete de Python
-    const pythonPath = execSync('which python').toString().trim()
+    // Obtener la ruta completa del intérprete de Python desde la variable de entorno PATH
+    const pythonPath = ENV.PATH.split(path.delimiter).find((path: any) =>
+        path.endsWith('/python')
+    )
+
+    if (!pythonPath) {
+        console.error('No se encontró la ruta del intérprete de Python')
+        response.status(500).json({ error: 'Internal server error' })
+        return
+    }
 
     // Utilizar la variable `pythonPath` en tu código
     console.log(`Ruta de Python: ${pythonPath}`)
     // ...resto de tu código utilizando `pythonPath`...
 
-    const cmd = spawn('python', [
+    const cmd = spawn(pythonPath, [
         path.join(process.cwd(), 'scripts/transcribe.py'),
         video_id || ''
     ])
